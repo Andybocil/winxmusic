3d0a9fda066e17ee5d4f0b975a214483e609293e
memex2711 <projectmemex7@gmail.com>
2025-02-13 03:44:26 +0700

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,21 +1,14 @@
-#
-# Copyright (C) 2021-2022 by Alexa_Help@Github, < https://github.com/Jankarikiduniya >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
-
-# Kanged By © @Dr_Asad_Ali
-# Rocks © @Shayri_Music_Lovers
-# Owner Asad Ali
-# Harshit Sharma
-# All rights reserved. © Alisha © Alexa © Yukki
-
-
 import os
+import sys
 from typing import List
 
 import yaml
 
 languages = {}
 commands = {}
+
+
+languages_present = {}
 
 
 def get_command(value: str) -> List:
@@ -35,17 +28,27 @@ for filename in os.listdir(r"./strings"):
 
 
 for filename in os.listdir(r"./strings/langs/"):
-    if "en" not in languages:
-        languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
+    if "id" not in languages:
+        languages["id"] = yaml.safe_load(
+            open(r"./strings/langs/id.yml", encoding="utf8")
         )
+        languages_present["id"] = languages["id"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "en":
+        if language_name == "id":
             continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
-        for item in languages["en"]:
+        for item in languages["id"]:
             if item not in languages[language_name]:
-                languages[language_name][item] = languages["en"][item]
+                languages[language_name][item] = languages["id"][item]
+    try:
+        languages_present[language_name] = languages[language_name][
+            "name"
+        ]
+    except:
+        print(
+            "There is some issue with the language file inside bot. Please report it to the TeamYukki at @YukkiSupport on Telegram"
+        )
+        sys.exit()
