1931164d74a441cdaaa85de37b0dfbcd8e1ba1f7
memex2711 <projectmemex7@gmail.com>
2025-02-13 16:02:28 +0700

    fix

--- AmonMusic/plugins/admins/mute.py
+++ AmonMusic/plugins/admins/mute.py
@@ -1,24 +1,13 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AlexaMusic import app
-from AlexaMusic.core.call import Alexa
-from AlexaMusic.utils.database import is_muted, mute_on
-from AlexaMusic.utils.decorators import AdminRightsCheck
+from AmonMusic import app
+from AmonMusic.core.call import Amon
+from AmonMusic.utils.database import is_muted, mute_on
+from AmonMusic.utils.decorators import AdminRightsCheck
+from AmonMusic.utils.inline import close_markup
 
 # Commands
 MUTE_COMMAND = get_command("MUTE_COMMAND")
@@ -32,7 +21,7 @@ async def mute_admin(cli, message: Message, _, chat_id):
     if await is_muted(chat_id):
         return await message.reply_text(_["admin_5"], disable_web_page_preview=True)
     await mute_on(chat_id)
-    await Alexa.mute_stream(chat_id)
+    await Amon.mute_stream(chat_id)
     await message.reply_text(
-        _["admin_6"].format(message.from_user.mention), disable_web_page_preview=True
+        _["admin_6"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
