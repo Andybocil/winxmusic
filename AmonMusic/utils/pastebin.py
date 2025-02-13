d32ecc5567274250350c6ed0f3ca167ef58f797e
memex2711 <projectmemex7@gmail.com>
2025-02-14 02:17:17 +0700

    Memex

--- AmonMusic/utils/pastebin.py
+++ AmonMusic/utils/pastebin.py
@@ -1,15 +1,3 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 import aiohttp
 
 BASE = "https://batbin.me/"
@@ -25,7 +13,7 @@ async def post(url: str, *args, **kwargs):
         return data
 
 
-async def Alexabin(text):
+async def Amonbin(text):
     resp = await post(f"{BASE}api/v2/paste", data=text)
     if not resp["success"]:
         return
