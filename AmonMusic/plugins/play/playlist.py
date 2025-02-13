cade6114b7efb29580b39073b87c1d31d0212f56
memex2711 <projectmemex7@gmail.com>
2025-02-13 19:15:29 +0700

    Memex

--- AmonMusic/plugins/play/playlist.py
+++ AmonMusic/plugins/play/playlist.py
@@ -1,14 +1,3 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
 import os
 from random import randint
 
@@ -16,21 +5,21 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from AlexaMusic import Carbon, YouTube, app
-from AlexaMusic.utils.database import (
+from AmonMusic import Carbon, YouTube, app
+from AmonMusic.utils.database import (
     delete_playlist,
     get_playlist,
     get_playlist_names,
     save_playlist,
 )
-from AlexaMusic.utils.decorators.language import language, languageCB
-from AlexaMusic.utils.inline.playlist import (
+from AmonMusic.utils.decorators.language import language, languageCB
+from AmonMusic.utils.inline.playlist import (
     botplaylist_markup,
     get_playlist_markup,
     warning_markup,
 )
-from AlexaMusic.utils.pastebin import Alexabin
-from AlexaMusic.utils.stream.stream import stream
+from AmonMusic.utils.pastebin import Amonbin
+from AmonMusic.utils.stream.stream import stream
 from config import BANNED_USERS, SERVER_PLAYLIST_LIMIT
 from strings import get_command
 
@@ -57,7 +46,7 @@ async def check_playlist(client, message: Message, _):
         count += 1
         msg += f"\n\n{count}- {title[:70]}\n"
         msg += _["playlist_5"].format(duration)
-    link = await Alexabin(msg)
+    link = await Amonbin(msg)
     lines = msg.count("\n")
     if lines >= 17:
         car = os.linesep.join(msg.split(os.linesep)[:17])
