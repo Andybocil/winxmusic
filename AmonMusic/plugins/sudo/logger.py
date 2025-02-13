49769fa4517fcb9e373860f0e122faca1adfb1d0
memex2711 <projectmemex7@gmail.com>
2025-02-13 23:37:12 +0700

    Memex

--- AmonMusic/plugins/sudo/logger.py
+++ AmonMusic/plugins/sudo/logger.py
@@ -1,23 +1,11 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 from pyrogram import filters
 
 import config
 from strings import get_command
-from AlexaMusic import app
-from AlexaMusic.misc import SUDOERS
-from AlexaMusic.utils.database import add_off, add_on
-from AlexaMusic.utils.decorators.language import language
+from AmonMusic import app
+from AmonMusic.misc import SUDOERS
+from AmonMusic.utils.database import add_off, add_on
+from AmonMusic.utils.decorators.language import language
 
 # Commands
 LOGGER_COMMAND = get_command("LOGGER_COMMAND")
