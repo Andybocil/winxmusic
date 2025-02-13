b8f9e0dd4ca9d1c7604ff75903c5f2b191da76ba
memex2711 <projectmemex7@gmail.com>
2025-02-13 16:55:00 +0700

    Memex

--- AmonMusic/plugins/bot/inline.py
+++ AmonMusic/plugins/bot/inline.py
@@ -1,15 +1,3 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 from pyrogram.types import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
@@ -18,8 +6,8 @@ from pyrogram.types import (
 from youtubesearchpython.__future__ import VideosSearch
 
 from config import BANNED_USERS, MUSIC_BOT_NAME
-from AlexaMusic import app
-from AlexaMusic.utils.inlinequery import answer
+from AmonMusic import app
+from AmonMusic.utils.inlinequery import answer
 
 
 @app.on_inline_query(~BANNED_USERS)
