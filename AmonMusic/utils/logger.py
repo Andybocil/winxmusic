13a2f03dfea757c031db1f4ea0a9f69ebb6ea84c
memex2711 <projectmemex7@gmail.com>
2025-02-14 02:16:14 +0700

    Memex

--- AmonMusic/utils/logger.py
+++ AmonMusic/utils/logger.py
@@ -1,19 +1,8 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
 from pyrogram.enums import ParseMode
 
 from config import LOG_GROUP_ID
-from AlexaMusic.utils.database import is_on_off
-from AlexaMusic import app
+from AmonMusic.utils.database import is_on_off
+from AmonMusic import app
 
 
 async def play_logs(message, streamtype):
