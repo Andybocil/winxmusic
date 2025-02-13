f7c9534a10f7742f024e2b3ebb3703c1cae71a53
memex2711 <projectmemex7@gmail.com>
2025-02-13 16:26:41 +0700

    Memex

--- AmonMusic/plugins/admins/unmute.py
+++ AmonMusic/plugins/admins/unmute.py
@@ -1,24 +1,12 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AlexaMusic import app
-from AlexaMusic.core.call import Alexa
-from AlexaMusic.utils.database import is_muted, mute_off
-from AlexaMusic.utils.decorators import AdminRightsCheck
+from AmonMusic import app
+from AmonMusic.core.call import Amon
+from AmonMusic.utils.database import is_muted, mute_off
+from AmonMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 UNMUTE_COMMAND = get_command("UNMUTE_COMMAND")
@@ -32,7 +20,7 @@ async def unmute_admin(Client, message: Message, _, chat_id):
     if not await is_muted(chat_id):
         return await message.reply_text(_["admin_7"], disable_web_page_preview=True)
     await mute_off(chat_id)
-    await Alexa.unmute_stream(chat_id)
+    await Amon.unmute_stream(chat_id)
     await message.reply_text(
-        _["admin_8"].format(message.from_user.mention), disable_web_page_preview=True
+        _["admin_8"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
