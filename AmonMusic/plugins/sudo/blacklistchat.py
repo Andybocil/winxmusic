0ed2e775adcd92456f10e747d53be294132abe50
memex2711 <projectmemex7@gmail.com>
2025-02-13 23:02:46 +0700

    Memex

--- AmonMusic/plugins/sudo/blacklistchat.py
+++ AmonMusic/plugins/sudo/blacklistchat.py
@@ -1,24 +1,12 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AlexaMusic import app
-from AlexaMusic.misc import SUDOERS
-from AlexaMusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
-from AlexaMusic.utils.decorators.language import language
+from AmonMusic import app
+from AmonMusic.misc import SUDOERS
+from AmonMusic.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
+from AmonMusic.utils.decorators.language import language
 
 # Commands
 
