55ad980cfddf3ef7125bacf9d779663b2d47d899
memex2711 <projectmemex7@gmail.com>
2025-02-13 16:06:33 +0700

    Update pause.py

--- AmonMusic/plugins/admins/pause.py
+++ AmonMusic/plugins/admins/pause.py
@@ -1,24 +1,13 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
 from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AlexaMusic import app
-from AlexaMusic.core.call import Alexa
-from AlexaMusic.utils.database import is_music_playing, music_off
-from AlexaMusic.utils.decorators import AdminRightsCheck
+from AmonMusic import app
+from AmonMusic.core.call import Amon
+from AmonMusic.utils.database import is_music_playing, music_off
+from AmonMusic.utils.decorators import AdminRightsCheck
+from AmonMusic.utils.inline import close_markup
 
 # Commands
 PAUSE_COMMAND = get_command("PAUSE_COMMAND")
@@ -32,7 +21,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"], disable_web_page_preview=True)
     await music_off(chat_id)
-    await Alexa.pause_stream(chat_id)
+    await Amon.pause_stream(chat_id)
     await message.reply_text(
-        _["admin_2"].format(message.from_user.mention), disable_web_page_preview=True
+        _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
