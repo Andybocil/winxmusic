0299d1240cafe01919dfa363c7507db376443fd6
memex2711 <projectmemex7@gmail.com>
2025-02-14 17:38:01 +0700

    Memex

--- AmonMusic/plugins/bot/help.py
+++ AmonMusic/plugins/bot/help.py
@@ -3,21 +3,29 @@ from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from config import BANNED_USERS
+from config import BANNED_USERS, START_IMG_URL
 from strings import get_command, get_string, helpers
 from AmonMusic import app
 from AmonMusic.misc import SUDOERS
-from AmonMusic.utils import help_pannel
+from AmonMusic.utils import first_page, second_page
 from AmonMusic.utils.database import get_lang, is_commanddelete_on
-from AmonMusic.utils.decorators.language import LanguageStart, languageCB
-from AmonMusic.utils.inline.help import help_back_markup, private_help_panel
+from AmonMusic.utils.decorators.language import (LanguageStart,
+                                                  languageCB)
+from AmonMusic.utils.inline.help import (help_back_markup,
+                                          private_help_panel)
 
 ### Command
 HELP_COMMAND = get_command("HELP_COMMAND")
 
 
-@app.on_message(filters.command(HELP_COMMAND) & filters.private & ~BANNED_USERS)
-@app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
+@app.on_message(
+    filters.command(HELP_COMMAND)
+    & filters.private
+    & ~BANNED_USERS
+)
+@app.on_callback_query(
+    filters.regex("settings_back_helper") & ~BANNED_USERS
+)
 async def helper_private(
     client: app, update: Union[types.Message, types.CallbackQuery]
 ):
@@ -30,30 +38,36 @@ async def helper_private(
         chat_id = update.message.chat.id
         language = await get_lang(chat_id)
         _ = get_string(language)
-        keyboard = help_pannel(_, True)
-        if update.message.photo:
-            await update.message.delete()
-            await update.message.reply_text(_["help_1"], reply_markup=keyboard)
-        else:
-            await update.edit_message_text(_["help_1"], reply_markup=keyboard)
+        keyboard = first_page(_)
+        await update.edit_message_text(
+            _["help_1"], reply_markup=keyboard
+        )
     else:
-        chat_id = update.chat.id
-        if await is_commanddelete_on(update.chat.id):
-            try:
-                await update.delete()
-            except:
-                pass
-        language = await get_lang(chat_id)
+        try:
+            await update.delete()
+        except:
+            pass
+        language = await get_lang(update.chat.id)
         _ = get_string(language)
-        keyboard = help_pannel(_)
-        await update.reply_text(_["help_1"], reply_markup=keyboard)
+        keyboard = first_page(_)
+        await update.reply_photo(
+            photo=START_IMG_URL,
+            caption=_["help_1"],
+            reply_markup=keyboard,
+)
 
 
-@app.on_message(filters.command(HELP_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+    filters.command(HELP_COMMAND)
+    & filters.group
+    & ~BANNED_USERS
+)
 @LanguageStart
 async def help_com_group(client, message: Message, _):
     keyboard = private_help_panel(_)
-    await message.reply_text(_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard))
+    await message.reply_text(
+        _["help_2"], reply_markup=InlineKeyboardMarkup(keyboard)
+    )
 
 
 @app.on_callback_query(filters.regex("help_callback") & ~BANNED_USERS)
@@ -62,18 +76,6 @@ async def helper_cb(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     cb = callback_data.split(None, 1)[1]
     keyboard = help_back_markup(_)
-    if cb == "hb5":
-        if CallbackQuery.from_user.id not in SUDOERS:
-            return await CallbackQuery.answer(
-                "ᴏɴʟʏ ғᴏʀ ᴏᴡɴᴇʀ ᴀɴᴅ sᴜᴅᴏᴇʀs", show_alert=True
-            )
-        else:
-            await CallbackQuery.edit_message_text(helpers.HELP_5, reply_markup=keyboard)
-            return await CallbackQuery.answer()
-    try:
-        await CallbackQuery.answer()
-    except:
-        pass
     if cb == "hb1":
         await CallbackQuery.edit_message_text(helpers.HELP_1, reply_markup=keyboard)
     elif cb == "hb2":
@@ -134,8 +136,10 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(helpers.HELP_29, reply_markup=keyboard)
     elif cb == "hb30":
         await CallbackQuery.edit_message_text(helpers.HELP_30, reply_markup=keyboard)
+      
 
-@app.on_callback_query(filters.regex("AmonSecpg") & ~BANNED_USERS)
+
+@app.on_callback_query(filters.regex("AuputSecpg") & ~BANNED_USERS)
 @languageCB
 async def first_pagexx(client, CallbackQuery, _):
     menu_next = second_page(_)
@@ -144,4 +148,3 @@ async def first_pagexx(client, CallbackQuery, _):
         return
     except:
         return
-        
