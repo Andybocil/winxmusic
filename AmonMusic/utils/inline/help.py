95c7bba2e2fe5688bd584b691c14278b0bf8d464
memex2711 <projectmemex7@gmail.com>
2025-02-22 16:53:52 +0700

    Update help.py

--- AmonMusic/utils/inline/help.py
+++ AmonMusic/utils/inline/help.py
@@ -6,7 +6,7 @@ from AmonMusic import app
 
 
 def first_page(_):
-	controll_button = [InlineKeyboardButton(text="๏ ᴍᴇɴᴜ ๏", callback_data=f"settingsback_helper"), InlineKeyboardButton(text="๏ ɴᴇxᴛ ๏", callback_data=f"AuputSecpg")]
+	controll_button = [InlineKeyboardButton(text="↩️ ᴍᴇɴᴜ", callback_data=f"settingsback_helper"), InlineKeyboardButton(text="ɴᴇxᴛ ➡️", callback_data=f"AuputSecpg")]
 	first_page_menu = InlineKeyboardMarkup(
 		[
 			[InlineKeyboardButton(text=_["H_B_1"], callback_data="help_callback hb1"), InlineKeyboardButton(text=_["H_B_2"], callback_data="help_callback hb2"),InlineKeyboardButton(text=_["H_B_3"], callback_data="help_callback hb3")],
@@ -21,7 +21,7 @@ def first_page(_):
 
 
 def second_page(_):
-	controll_button = [InlineKeyboardButton(text="๏ ʙᴀᴄᴋ ๏", callback_data=f"settings_back_helper")]
+	controll_button = [InlineKeyboardButton(text="↩️ ʙᴀᴄᴋ", callback_data=f"settings_back_helper")]
 	second_page_menu = InlineKeyboardMarkup(
 		[
 			[InlineKeyboardButton(text=_["H_B_16"], callback_data="help_callback hb16"), InlineKeyboardButton(text=_["H_B_17"], callback_data="help_callback hb17"),InlineKeyboardButton(text=_["H_B_18"], callback_data="help_callback hb18")],
