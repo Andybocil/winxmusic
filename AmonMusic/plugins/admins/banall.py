9cad8c1b7878a3702932b9b9ad755b09f167ac94
Andybocil <158800993+Andybocil@users.noreply.github.com>
2025-06-19 16:33:48 +0700

    Update banall.py

--- AmonMusic/plugins/admins/banall.py
+++ AmonMusic/plugins/admins/banall.py
@@ -3,27 +3,32 @@ from AmonMusic import app
 from config import OWNER_ID
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import ChatMemberAdministrator
 from AmonMusic.misc import SUDOERS
 
-BOT_ID = app.me.id 
+BOT_ID = app.me.id  
 
-@app.on_message(filters.command("duarrmemek") & SUDOERS)
+@app.on_message(filters.command("banalll") & SUDOERS)
 async def ban_all(_, msg):
     chat_id = msg.chat.id
     banned = 0
     skipped = 0
 
-  
     bot_member = await app.get_chat_member(chat_id, BOT_ID)
+
+   
+    if bot_member.status != ChatMemberStatus.ADMINISTRATOR or not isinstance(bot_member, ChatMemberAdministrator):
+        return await msg.reply_text("❌ Bot bukan admin atau tidak bisa membanned anggota.")
+
     if not bot_member.can_restrict_members:
-        return await msg.reply_text("❌ Bot tidak memiliki izin untuk membanned anggota di grup ini.")
+        return await msg.reply_text("❌ Bot tidak punya izin Ban Users di grup ini.")
 
     async for member in app.get_chat_members(chat_id):
         user_id = member.user.id
 
         if (
-            member.status in (ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER)
-            or user_id in [BOT_ID, msg.from_user.id, OWNER_ID]
+            member.status in (ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER) or
+            user_id in [BOT_ID, msg.from_user.id, OWNER_ID]
         ):
             skipped += 1
             continue
